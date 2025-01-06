1406f3db5e03358e25c8098218db70e51c2324f2
Hasan Şişik <sisik.hsn@gmail.com>
2025-01-06 23:05:09 +0300

    dashboardh kurum bilgileri client edit

--- components/forms/user-profile-stepper/create-profile.tsx
+++ components/forms/user-profile-stepper/create-profile.tsx
@@ -4,7 +4,7 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger
-} from '@/components/ui/accordion';
+} from '@/components/ui/accordionHeader';
 import { Button } from '@/components/ui/button';
 import {
   Form,
--- components/tables/user-tables/client.tsx
+++ components/tables/user-tables/client.tsx
@@ -1,12 +1,26 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { DataTable } from '@/components/ui/data-table';
-import { Heading } from '@/components/ui/heading';
-import { Separator } from '@/components/ui/separator';
-import { User } from '@/constants/data';
-import { Plus } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { columns } from './columns';
+"use client";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { User } from "@/constants/data";
+import {
+  Plus,
+  Building2,
+  Hash,
+  Phone,
+  MapPin,
+  Calendar,
+  Users,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+import { columns } from "./columns";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordionHeader";
 
 interface ProductsClientProps {
   data: User[];
@@ -17,20 +31,83 @@ export const UserClient: React.FC<ProductsClientProps> = ({ data }) => {
 
   return (
     <>
-      <div className="flex items-start justify-between">
-        <Heading
-          title={`Users (${data.length})`}
-          description="Manage users (Client side table functionalities.)"
-        />
-        <Button
-          className="text-xs md:text-sm"
-          onClick={() => router.push(`/dashboard/user/new`)}
-        >
-          <Plus className="mr-2 h-4 w-4" /> Add New
-        </Button>
+      <div className="flex flex-col items-start justify-between bg-sidebar p-10 gap-4 border rounded-md border-slate-450">
+        <Heading title={`99943021`} description="Kurum Bilgileri" />
+        <div className="w-full flex flex-col gap-6">
+          <div className="flex flex-col md:flex-row gap-4 justify-between">
+            <div className="flex-1">
+              <span className="text-xs text-gray-500 flex items-center gap-2">
+                <Building2 className="w-4 h-4" />
+                KURUM ADI
+              </span>
+              <span className="font-medium break-words block">
+                ÖZEL AKTİF GÖRSEL MOTORLU TAŞIT SÜRÜCÜLERİ KURSU
+              </span>
+            </div>
+            <div className="flex-1 md:text-center">
+              <span className="text-xs text-gray-500 flex items-center gap-2 md:justify-center">
+                <Hash className="w-4 h-4" />
+                KURUM KODU
+              </span>
+              <span className="font-medium block">99943021</span>
+            </div>
+            <div className="flex-1 md:text-right">
+              <span className="text-xs text-gray-500 flex items-center gap-2 md:justify-end">
+                <Phone className="w-4 h-4" />
+                KURUM TELEFON
+              </span>
+              <span className="font-medium block">2164501515</span>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row gap-4 justify-between">
+            <div className="flex-1">
+              <span className="text-xs text-gray-500 flex items-center gap-2">
+                <MapPin className="w-4 h-4" />
+                KURUM ADRES
+              </span>
+              <span className="font-medium break-words block">
+                KAYIŞDAĞI MAH. USLU CAD. NO: 29 İÇ KAPI NO: 1 ATAŞEHİR /
+                İSTANBUL
+              </span>
+            </div>
+            <div className="flex-1 md:text-center">
+              <span className="text-xs text-gray-500 flex items-center gap-2 md:justify-center">
+                <Calendar className="w-4 h-4" />
+                AÇILMA TARİHİ
+              </span>
+              <span className="font-medium block">17/07/2006</span>
+            </div>
+            <div className="flex-1 md:text-right">
+              <span className="text-xs text-gray-500 flex items-center gap-2 md:justify-end">
+                <Users className="w-4 h-4" />
+                BİNA KONTEJYAN
+              </span>
+              <span className="font-medium block">54</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <Separator />
-      <DataTable searchKey="name" columns={columns} data={data} />
+      <Accordion type="single" collapsible className="w-full flex flex-col gap-4 pt-5">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Ehliyet Sınıfları</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Araç Bilgileri</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Derslik Bilgileri</AccordionTrigger>
+          <AccordionContent>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </>
   );
 };
