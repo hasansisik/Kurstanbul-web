fb21e77988650d1964ee5885693bba925cddcdd2
Hasan Şişik <sisik.hsn@gmail.com>
2025-01-08 20:03:50 +0300

    menu

---
+++ app/dashboard/soon/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/components/ui/button';
+
+export default function Soon() {
+  const router = useRouter();
+
+  return (
+    <div className="absolute left-1/2 top-1/2 mb-16 -translate-x-1/2 -translate-y-1/2 items-center justify-center text-center">
+      <span className="bg-gradient-to-b from-foreground to-transparent bg-clip-text text-[10rem] font-extrabold leading-none text-transparent">
+        YAKINDA...
+      </span>
+      <h2 className="font-heading my-2 text-2xl font-bold">
+        Bu özellik yakında sizlerle
+      </h2>
+      <p>
+      Üzgünüz, aradığınız sayfa mevcut değil.
+      </p>
+      <div className="mt-8 flex justify-center gap-2">
+        <Button onClick={() => router.back()} variant="default" size="lg">
+          Geri Dön
+        </Button>
+        <Button
+          onClick={() => router.push('/dashboard')}
+          variant="ghost"
+          size="lg"
+        >
+          Anasayfaya Dön
+        </Button>
+      </div>
+    </div>
+  );
+}
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -42,23 +42,125 @@ const data = {
   navMain: [
     {
       title: "Kurstanbul Mobil",
-      url: "/dashboard/user",
+      url: "/dashboard/soon",
       icon: Smartphone,
       isActive: true,
       items: [
+        {
+          title: "Kursum",
+          url: "/dashboard/soon",
+        },
+        {
+          title: "Personellerim",
+          url: "/dashboard/soon",
+        },
+        {
+          title: "Değerlendirmelerim",
+          url: "/dashboard/soon",
+        },
+        {
+          title:"Mevcut Ehliyet Sınıfları",
+          url:"/dashboard/soon"
+        },
         {
           title: "Uygulama Ders Kayıt",
           url: "/dashboard/mobile/class",
         },
       ],
     },
+    {
+      title: "Kurum İşlemleri",
+      url: "/dashboard/courseProcess",
+      icon: Home,
+      items: [
+        {
+          title: "Kurum Bilgisi",
+          url: "/dashboard/courseProcess",
+        },
+        {
+          title: "Kurum Araç",
+          url: "/dashboard/soon",
+        },
+        {
+          title: "Kurum Derslik",
+          url: "/dashboard/soon",
+        },
+        {
+          title: "Kurum Personelleri",
+          url: "/dashboard/soon",
+        },
+
+      ],
+    },
+    {
+      title: "Kurum Aday Kayıt",
+      url: "/dashboard/soon",
+      icon: User,
+      items: [
+        {
+          title: "Aday Dönem Kayıt",
+          url: "/dashboard/soon",
+        },
+        {
+          title: "Aday Evrak Kayıt",
+          url: "/dashboard/soon",
+        },
+        {
+          title: "Aday Listeleme/Kontrol",
+          url: "/dashboard/soon",
+        },
+        {
+          title: "Aday Durum Görüntüleme",
+          url: "/dashboard/soon",
+        },
+      ],
+    },
+    {
+      title: "Ayarlar",
+      url: "/dashboard/settings",
+      icon: Settings2,
+      items: [
+        {
+          title: "Profil",
+          url: "/dashboard/settings",
+        },
+        {
+          title: "Hesap",
+          url: "/dashboard/settings/account",
+        },
+        {
+          title: "Görünüm",
+          url: "/dashboard/settings/appearance",
+        },
+        {
+          title: "Bildirimler",
+          url: "/dashboard/settings/notifications",
+        },
+      ],
+    },
   ],
   projects: [
     {
-      name: "Uygulama Ders Kayıt",
-      url: "/dashboard/mobile/class",
+      name: "Aday Dönem Kayıt",
+      url: "/dashboard/soon",
+      icon: Map,
+    },
+    {
+      name: "MEBBİS Aktarım",
+      url: "/dashboard/soon",
       icon: Frame,
-    }, 
+    },
+    {
+      name: "Mağaza",
+      url: "/dashboard/soon",
+      icon: Store,
+    },
+    {
+      name: "Kurum Bilgisi",
+      url: "/dashboard/soon",
+      icon: Map,
+    },
+   
   ],
 }
 
