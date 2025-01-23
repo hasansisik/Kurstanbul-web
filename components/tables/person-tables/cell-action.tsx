303b0b0ee4f4324606c9075ce60c3e3d4f37d334
Hasan Şişik <sisik.hsn@gmail.com>
2025-01-23 20:20:18 +0300

    delete2

--- components/tables/person-tables/cell-action.tsx
+++ components/tables/person-tables/cell-action.tsx
@@ -8,7 +8,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
-import { Class } from '@/constants/data';
+import { Person } from '@/constants/data';
 import { Edit, MoreHorizontal, Trash } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 import { useState } from 'react';
